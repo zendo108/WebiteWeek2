6e85b0475a6c24dbdbe97325882d359dad347b61
Ivan Aranda <IvanAranda@iMac.local>
2019-06-29 19:18:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# WebiteWeek2
---
+++ public_html/js/footer.js
@@ -0,0 +1,29 @@
+var footer = '<footer class="footer">'+
+'            <div class="container">'+
+'                <div class="row">'+
+'                  <div class="col-sm-4">'+
+'                    <h6>Contact</h6>'+
+'                    <p class=" text-info">Ivan Aranda ivan@allelectricalwork.com</p>'+
+'                  </div>'+
+'                  <div class="col-sm-4">'+
+'                    <h6>External Links</h6>'+
+'                    <ul>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                    </ul>'+
+'                  </div>'+
+'                  <div class="col-sm-4">'+
+'                    <h6>Social Media</h6>        '+
+'                    <ul>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                        <li><a href="#">Some link</a></li>'+
+'                    </ul>'+
+'                  </div>'+
+'                </div>'+
+'                <p class="text-muted">Created by Ivan Aranda Copyright&copy; <script>document.write(new Date().getFullYear())</script></p>'+
+'            </div>'+
+'        </footer>';
